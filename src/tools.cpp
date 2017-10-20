7a4eccdfea70fc06c8516472a16310954fb69689
Peter Hwang <peterhwang.inbev@gmail.com>
2017-10-20 12:09:24 -0400

    added tool to calculate jacobian matrix

--- src/tools.cpp
+++ src/tools.cpp
@@ -40,4 +40,23 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  if ( x_state.size() != 4 ) { return Hj; }
+
+	double px = x_state(0);
+	double py = x_state(1);
+	double vx = x_state(2);
+  double vy = x_state(3);
+  
+	double c1 = px*px+py*py;
+	double c2 = sqrt(c1);
+	double c3 = (c1*c2);
+
+	if(fabs(c1) < 0.0001){ return Hj; }
+
+	//compute the Jacobian matrix
+	Hj << (px/c2), (py/c2), 0, 0,
+		    -(py/c1), (px/c1), 0, 0,
+		    py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+	return Hj;
 }
